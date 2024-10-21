956f57c9d7832a6f27ae860333eac93b85a6fd5c
Maverick <25895481+1nC0deWeTrust@users.noreply.github.com>
2024-10-21 09:55:09 +0800

    init proj.

---
+++ src/main/java/org/example/stumanagement/StuManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.stumanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuManagementApplication.class, args);
+    }
+
+}
